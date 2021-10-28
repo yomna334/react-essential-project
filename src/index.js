700c8c83baac269d908c3ed726d000b7eafccf20
yomna334 <yomnaayman987@gmail.com>
2021-10-28 15:17:41 -0700

    react-essential-project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+// React.createElement("h1",{style:{color:"blue"}},"hello"),
+React.createElement("ul",
+null,
+React.createElement("li",null,"bag"),
+React.createElement("li",null,"purse"),
+React.createElement("li",null,"backbag"),
+),
+  document.getElementById('root')
+
+);
