68a3511b101cac265211d6e83b11dae5bcc064b3
yomna334 <yomnaayman987@gmail.com>
2021-11-01 17:05:16 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+// function Header(props) {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//   return (
+//     <div>{props.name}'s header</div>
+//   )
+// }
+// const dishes = ["macroni,fried chicken",
+//   "fishes,rice",
+//   "meat,vegetables"]
+// const dishObjs = dishes.map((dish, id) => ({ id: id, title: dish }));
+// console.log(dishObjs);
+// function Main(props) {
 
+//   return (
+//     <ul style={{ textAlign: "left" }}>
+//       {props.dishes.map((dish) => (<li key={dish.id}>{dish.title}</li>))}</ul>
+//   )
+// }
+// function Footer(props) {
+
+//   return (
+//     <div>{props.date}</div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//       <Header name="ymna" />
+//       <Main dishes={dishObjs} />
+//       <Footer date={new Date().getFullYear()} />
+//     </div>
+//   );
+// }
+/////////////////////////////////////////////////////
+function SuperSecretComponent(){
+  return (<h1>Secret information for authorized users</h1>)
+  }
+  function RegularComponent(){
+    return (<h1>everyone can see this component</h1>)
+    }
+  function App(props) {
+    console.log(props.authorized)
+    // if(props.authorized){
+
+    //   return (
+    //     <SuperSecretComponent/>
+    //   );
+    // }
+    // else{
+
+    //   return(<RegularComponent/>)
+    // }
+    return(<>
+    {props.authorized? <SuperSecretComponent/>:<RegularComponent/>}
+
+    </>)};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+const [checkList]=["boots","tents","headLamp"];
+console.log([checkList])
 ReactDOM.render(
 // React.createElement("h1",{style:{color:"blue"}},"hello"),
-React.createElement("ul",
-null,
-React.createElement("li",null,"bag"),
-React.createElement("li",null,"purse"),
+// React.createElement("ul",
+// null,
+// React.createElement("li",null,"bag"),
+// React.createElement("li",null,"purse"),
+// )
 
-),
+<App authorized={false}/>
+,
   document.getElementById('root')
 
 );
